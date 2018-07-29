c2bd79e81a2adc1ede70ca6ddff7efebd5bdda9d
Krystian <kkuzminski@betacom.com.pl>
2018-07-29 20:20:05 +0200

    init commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,17 +1,3 @@
-function debounce(func, wait, immediate) {
-    let timeout;
-    return () => {
-        let context = this, args = arguments;
-        let later = function () {
-            timeout = null;
-            if (!immediate) func.apply(context, args);
-        };
-        let callNow = immediate && !timeout;
-        clearTimeout(timeout);
-        timeout = setTimeout(later, wait);
-        if (callNow) func.apply(context, args);
-    };
-}
 
 $('#container').imagesLoaded()
     .always(() => {
@@ -37,6 +23,9 @@ $('#container').imagesLoaded()
                 nav.removeClass(scrollClass);
             }
         };
+
+
+
 
         let link = $('.main-menu__link:not(".no-action")');
         link.on('click', function (e) {
